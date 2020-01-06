b9e4c84afd4d9333d866f795afc87f35e0ed6207
Matthias <hanisch.matthias@googlemail.com>
2020-01-06 13:32:26 +0100

    - fixed start of spring components for ft232h

--- trainsimulator-ft232h/src/main/java/io/mathan/trainsimulator/ft232h/Ft232hApplication.java
+++ trainsimulator-ft232h/src/main/java/io/mathan/trainsimulator/ft232h/Ft232hApplication.java
@@ -2,10 +2,12 @@ package io.mathan.trainsimulator.ft232h;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = {"io.mathan.trainsimulator.service", "io.mathan.trainsimulator.ft232h"})
 public class Ft232hApplication {
   public static void main(String[] args) {
     SpringApplication.run(Ft232hApplication.class, args);
